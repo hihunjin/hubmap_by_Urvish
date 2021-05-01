1b6b3b158b2614e074497fb986ba495af10836e6
hihunjin <hihunjin@hanmail.net>
2021-05-01 09:58:34 +0900

    initial

---
+++ 1024x1024.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# I provide a 256x256 image dataset for the initial prototyping. Based on the size of the detected features, I'd expect that the appropriate tile size for this data should be 1024x1024. However, it would be an overshoot for the initial model development. Therefore, I reduced the images by 4 times to 256x256.
+# 
+# * The corresponding dataset: https://www.kaggle.com/iafoss/hubmap-256x256
+# * The dataset with 512x512 tiles (reduction of 2): https://www.kaggle.com/iafoss/hubmap-512x512
+# * The dataset with 1024x1024 tiles (no resolution reduction): https://www.kaggle.com/iafoss/hubmap-1024x1024
+# 
+# 
+# * Update (11/17): remove gray background tiles based on saturation check
+# * Update (11/19): fix a bug with dimentions in cv2.resize, please use the latest version
+# * Update (3/9): rerun with the new data (load images part by part because of insufficient RAM for one of the new images)
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import tifffile as tiff
+import cv2
+import os
+from tqdm.notebook import tqdm
+import zipfile
+import rasterio
+from rasterio.windows import Window
+from torch.utils.data import Dataset
+import gc
+import os.path
+
+
+# In[ ]:
+
+
+sz = 1024   #the size of tiles
+reduce = 1 #reduce the original images by 4 times 
+MASKS = '../input/hubmap-enhanced-masks/all_masks_v3.csv'
+#DATA = '../input/hubmap-kidney-segmentation/train/'
+OUT_TRAIN = 'train.zip'
+OUT_MASKS = 'masks.zip'
+
+
+# In[ ]:
+
+
+#functions to convert encoding to mask and mask to encoding
+def enc2mask(encs, shape):
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for m,enc in enumerate(encs):
+        if isinstance(enc,np.float) and np.isnan(enc): continue
+        s = enc.split()
+        for i in range(len(s)//2):
+            start = int(s[2*i]) - 1
+            length = int(s[2*i+1])
+            img[start:start+length] = 1 + m
+    return img.reshape(shape).T
+
+def mask2enc(mask, n=1):
+    pixels = mask.T.flatten()
+    encs = []
+    for i in range(1,n+1):
+        p = (pixels == i).astype(np.int8)
+        if p.sum() == 0: encs.append(np.nan)
+        else:
+            p = np.concatenate([[0], p, [0]])
+            runs = np.where(p[1:] != p[:-1])[0] + 1
+            runs[1::2] -= runs[::2]
+            encs.append(' '.join(str(x) for x in runs))
+    return encs
+
+df_masks = pd.read_csv(MASKS).set_index('id')
+df_masks
+
+
+# In[ ]:
+
+
+#one of the new images cannot be loaded into 16GB RAM
+#use rasterio to load image part by part
+#using a dataset similar to my submission kernel
+
+s_th = 40  #saturation blancking threshold
+p_th = 1000*(sz//256)**2 #threshold for the minimum number of pixels
+
+
+class HuBMAPDataset(Dataset):
+    def __init__(self, idx, sz=sz, reduce=reduce, encs=None):
+        
+        if os.path.isfile(os.path.join('../input/hubmap-kidney-segmentation/train/',idx+'.tiff')):
+            f = os.path.join('../input/hubmap-kidney-segmentation/train/',idx+'.tiff')
+        elif os.path.isfile(os.path.join('../input/hubmap-kidney-segmentation/test/',idx+'.tiff')):
+            f = os.path.join('../input/hubmap-kidney-segmentation/test/',idx+'.tiff')
+        elif os.path.isfile(os.path.join('../input/hubmap-external-data/external/',idx+'.tif')):
+            f = os.path.join('../input/hubmap-external-data/external/',idx+'.tif')
+        else:
+            print ("File not exist")
+        print(f)
+        
+        self.data = rasterio.open(f,num_threads='all_cpus')
+        
+        # some images have issues with their format 
+        # and must be saved correctly before reading with rasterio
+        if self.data.count != 3:
+            subdatasets = self.data.subdatasets
+            self.layers = []
+            if len(subdatasets) > 0:
+                for i, subdataset in enumerate(subdatasets, 0):
+                    self.layers.append(rasterio.open(subdataset))
+        self.shape = self.data.shape
+        self.reduce = reduce
+        self.sz = reduce*sz
+        self.pad0 = (self.sz - self.shape[0]%self.sz)%self.sz
+        self.pad1 = (self.sz - self.shape[1]%self.sz)%self.sz
+        self.n0max = (self.shape[0] + self.pad0)//self.sz
+        self.n1max = (self.shape[1] + self.pad1)//self.sz
+        self.mask = enc2mask(encs,(self.shape[1],self.shape[0])) if encs is not None else None
+        
+    def __len__(self):
+        return self.n0max*self.n1max
+    
+    def __getitem__(self, idx):
+        # the code below may be a little bit difficult to understand,
+        # but the thing it does is mapping the original image to
+        # tiles created with adding padding (like in the previous version of the kernel)
+        # then the tiles are loaded with rasterio
+        # n0,n1 - are the x and y index of the tile (idx = n0*self.n1max + n1)
+        n0,n1 = idx//self.n1max, idx%self.n1max
+        # x0,y0 - are the coordinates of the lower left corner of the tile in the image
+        # negative numbers correspond to padding (which must not be loaded)
+        x0,y0 = -self.pad0//2 + n0*self.sz, -self.pad1//2 + n1*self.sz
+
+        # make sure that the region to read is within the image
+        p00,p01 = max(0,x0), min(x0+self.sz,self.shape[0])
+        p10,p11 = max(0,y0), min(y0+self.sz,self.shape[1])
+        img = np.zeros((self.sz,self.sz,3),np.uint8)
+        mask = np.zeros((self.sz,self.sz),np.uint8)
+        # mapping the loade region to the tile
+        if self.data.count == 3:
+            img[(p00-x0):(p01-x0),(p10-y0):(p11-y0)] = np.moveaxis(self.data.read([1,2,3],
+                window=Window.from_slices((p00,p01),(p10,p11))), 0, -1)
+        else:
+            for i,layer in enumerate(self.layers):
+                img[(p00-x0):(p01-x0),(p10-y0):(p11-y0),i] =                  layer.read(1,window=Window.from_slices((p00,p01),(p10,p11)))
+        if self.mask is not None: mask[(p00-x0):(p01-x0),(p10-y0):(p11-y0)] = self.mask[p00:p01,p10:p11]
+        
+        if self.reduce != 1:
+            img = cv2.resize(img,(self.sz//reduce,self.sz//reduce),
+                             interpolation = cv2.INTER_AREA)
+            mask = cv2.resize(mask,(self.sz//reduce,self.sz//reduce),
+                             interpolation = cv2.INTER_NEAREST)
+        #check for empty imges
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h,s,v = cv2.split(hsv)
+        #return -1 for empty images
+        return img, mask, (-1 if (s>s_th).sum() <= p_th or img.sum() <= p_th else idx)
+
+
+# In[ ]:
+
+
+x_tot,x2_tot = [],[]
+with zipfile.ZipFile(OUT_TRAIN, 'w') as img_out, zipfile.ZipFile(OUT_MASKS, 'w') as mask_out:
+    for index, encs in tqdm(df_masks.iterrows(),total=len(df_masks)):
+        #image+mask dataset
+        ds = HuBMAPDataset(index,encs=encs)
+        for i in range(len(ds)):
+            im,m,idx = ds[i]
+            if idx < 0: continue
+                
+            x_tot.append((im/255.0).reshape(-1,3).mean(0))
+            x2_tot.append(((im/255.0)**2).reshape(-1,3).mean(0))
+            
+            #write data   
+            im = cv2.imencode('.png',cv2.cvtColor(im, cv2.COLOR_RGB2BGR))[1]
+            img_out.writestr(f'{index}_{idx:04d}.png', im)
+            m = cv2.imencode('.png',m)[1]
+            mask_out.writestr(f'{index}_{idx:04d}.png', m)
+        
+#image stats
+img_avr =  np.array(x_tot).mean(0)
+img_std =  np.sqrt(np.array(x2_tot).mean(0) - img_avr**2)
+print('mean:',img_avr, ', std:', img_std)
+
+
+# In[ ]:
+
+
+columns, rows = 4,4
+idx0 = 20
+fig=plt.figure(figsize=(columns*4, rows*4))
+with zipfile.ZipFile(OUT_TRAIN, 'r') as img_arch,      zipfile.ZipFile(OUT_MASKS, 'r') as msk_arch:
+    fnames = sorted(img_arch.namelist())[8:]
+    for i in range(rows):
+        for j in range(columns):
+            idx = i+j*columns
+            img = cv2.imdecode(np.frombuffer(img_arch.read(fnames[idx0+idx]), 
+                                             np.uint8), cv2.IMREAD_COLOR)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            mask = cv2.imdecode(np.frombuffer(msk_arch.read(fnames[idx0+idx]), 
+                                              np.uint8), cv2.IMREAD_GRAYSCALE)
+    
+            fig.add_subplot(rows, columns, idx+1)
+            plt.axis('off')
+            plt.imshow(Image.fromarray(img))
+            plt.imshow(Image.fromarray(mask), alpha=0.2)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
